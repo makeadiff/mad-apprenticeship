e4da0517823d946207ca72b09ee3f16ae83c78df
Binny V A <binnyva@gmail.com>
2021-09-12 17:44:29 +0530

    Showcase Ready

--- scripts/join.js
+++ scripts/join.js
@@ -2,6 +2,53 @@
 
 import { common } from './common.js';
 
+const BANGALORE_CITY_ID = 1;
+const MANGALORE_CITY_ID = 2;
+const TRIVANDRUM_CITY_ID = 3;
+const MUMBAI_CITY_ID = 4;
+const PUNE_CITY_ID = 5;
+const CHENNAI_CITY_ID = 6;
+const VELLORE_CITY_ID = 8;
+const COCHIN_CITY_ID = 10;
+const HYDERABAD_CITY_ID = 11;
+const DELHI_CITY_ID = 12;
+const CHANDIGARH_CITY_ID = 13;
+const NAGPUR_CITY_ID = 15;
+const COIMBATORE_CITY_ID = 16;
+const VIZAG_CITY_ID = 17;
+const VIJAYAWADA_CITY_ID = 18;
+const GWALIOR_CITY_ID = 19;
+const LUCKNOW_CITY_ID = 20;
+const BHOPAL_CITY_ID = 21;
+const MYSORE_CITY_ID = 22;
+const GUNTUR_CITY_ID = 23;
+const AHMEDABAD_CITY_ID = 24;
+const DEHRADUN_CITY_ID = 25;
+
+const programs_in_city = {}
+programs_in_city[BANGALORE_CITY_ID] = ['Foundation Skill Volunteer(5th-7th)', 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[CHENNAI_CITY_ID] = ['Foundation Skill Volunteer(5th-7th)', 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[COIMBATORE_CITY_ID] = ['Foundation Skill Volunteer(5th-7th)', 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[DELHI_CITY_ID] = ['Foundation Skill Volunteer(5th-7th)', 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[MUMBAI_CITY_ID] = ['Foundation Skill Volunteer(5th-7th)', 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[AHMEDABAD_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[BHOPAL_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[CHANDIGARH_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[COCHIN_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[DEHRADUN_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[GUNTUR_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[GWALIOR_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[HYDERABAD_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[LUCKNOW_CITY_ID] = [ 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[MANGALORE_CITY_ID] = [ 'Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[MYSORE_CITY_ID] = ['Foundation Skill Volunteer(5th-7th)', 'Academic Support Volunteer(5th-10th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[NAGPUR_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[PUNE_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[TRIVANDRUM_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[VELLORE_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[VIJAYAWADA_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Academic Support Volunteer(11th-12th)', 'Wingman(11th,12th)', 'Wingman(Undergrads and Graduates)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+programs_in_city[VIZAG_CITY_ID] = ['Academic Support Volunteer(5th-10th)', 'Fundraising Volunteer', 'Finance Volunteer', 'Humman Capital Volunteer', 'Campaigns Volunteer'];
+
 function handleResponse(ret) {
   document.querySelector("#loading").style.display = 'none';
   if (ret.status === "fail") {
@@ -56,12 +103,21 @@ function setJobStatus(e) {
 
 function setVolExp(e) {
   var status = e.target.value;
-  if(status == "yes") {
+  if(status == "Yes") {
     document.querySelector("#volunteering_experience_details").style.display = 'block';
   } else {
     document.querySelector("#volunteering_experience_details").style.display = 'none';
   }
-} 
+}
+
+function setAvailableProgramsInCity(e) {
+  const city_id = e.target.value;
+  var programs = "<option value=''>Choose first preference for role</option>";
+  programs += programs_in_city[city_id].map(program => "<option value='" + program + "'>" + program + "</option>").join("");
+
+  document.querySelector("#applied_role").innerHTML = programs;
+  document.querySelector("#applied_role_secondary").innerHTML = programs;
+}
 
 /* Show page when ready */
 function onReady() {
@@ -76,6 +132,7 @@ function onReady() {
 
   document.querySelector("#job_status").addEventListener('change', setJobStatus);
   document.querySelector("#volunteering_experience").addEventListener('change', setVolExp);
+  document.querySelector("#city_id").addEventListener('change', setAvailableProgramsInCity);
 
   document.getElementById('form').addEventListener('submit', submitForm);
   common.init();
--- system/join.php
+++ system/join.php
@@ -9,6 +9,22 @@ if(empty($data['name'])) {
 	$data = json_decode($json, true);
 }
 
+// Data edit before API Push...
+// If people mark volunteer experiance as 'yes' and they didn't write anything, that should be sent to API
+if(!empty($data['volunteering_experience'])) {
+	if($data['volunteering_experience'] == 'Yes' and !empty($data['volunteering_experience_details'])) {
+		$data['volunteering_experience'] = $data['volunteering_experience_details'];
+	}
+}
+
+// Why MAD should be joined and send as a json string
+if(!empty($data['why_mad_other'])) {
+	$data['why_mad'] = "Other: " . $data['why_mad_other']; 
+}
+
+var_dump($data);
+exit;
+
 $client = new Client(['http_errors' => false]); //GuzzleHttp\Client
 $response = '';
 try {
@@ -20,10 +36,16 @@ try {
 			"phone" 	=> $data['phone'],
 			"city_id" 	=> $data['city_id'],
 			"source" 	=> $data['source'],
-			"profile" 	=> isset($data['profile']) ? $$data['profile'] : '',
+			"applied_role" 	=> isset($data['applied_role']) ? $data['applied_role'] : '',
+			"applied_role_secondary" 	=> isset($data['applied_role_secondary']) ? $data['applied_role_secondary'] : '',
+			"company" 	=> isset($data['company']) ? $data['company'] : '',
+			"edu_institution" 	=> isset($data['edu_institution']) ? $data['edu_institution'] : '',
+			"edu_course"=> isset($data['edu_course']) ? $data['edu_course'] : '',
+			"edu_year" 	=> isset($data['edu_year']) ? $data['edu_year'] : '',
 			"address" 	=> isset($data['address']) ? $data['address'] : '',
 			"job_status"=> $data['job_status'],
 			"why_mad" 	=> $data['why_mad'],
+			"volunteering_experience" 	=> $data['volunteering_experience'],
 			"campaign" 	=> isset($data['campaign']) ? $data['campaign'] : '',
 			'user_type'	=> 'applicant',
 			"password" 	=> "pass"
