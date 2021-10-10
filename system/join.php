929e355f24f1fe72288b1b84e2a5ed8a7fac67b0
Binny V A <binnyva@gmail.com>
2021-10-10 10:42:24 +0530

    Changed city > Vertical mapping. Closed many because requirements filled

--- system/join.php
+++ system/join.php
@@ -4,22 +4,22 @@ use GuzzleHttp\Client;
 
 $data = $_REQUEST;
 if(empty($data['name'])) {
-	// Takes raw data from the request
-	$json = file_get_contents('php://input');
-	$data = json_decode($json, true);
+    // Takes raw data from the request
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
 }
 
 // Data edit before API Push...
 // If people mark volunteer experiance as 'yes' and they didn't write anything, that should be sent to API
 if(!empty($data['volunteering_experience'])) {
-	if($data['volunteering_experience'] == 'Yes' and !empty($data['volunteering_experience_details'])) {
-		$data['volunteering_experience'] = $data['volunteering_experience_details'];
-	}
+    if($data['volunteering_experience'] == 'Yes' and !empty($data['volunteering_experience_details'])) {
+        $data['volunteering_experience'] = $data['volunteering_experience_details'];
+    }
 }
 
 // Why MAD should be joined and send as a json string
 if(!empty($data['why_mad_other'])) {
-	$data['why_mad'] = "Other: " . $data['why_mad_other']; 
+    $data['why_mad'] = "Other: " . $data['why_mad_other']; 
 }
 
 // var_dump($data);exit;
@@ -30,26 +30,26 @@ $log = false;
 try {
     $result = $client->post('https://makeadiff.in/api/v1/users', [
         'form_params' => [
-			"name" 		=> $data['name'],
-			"sex" 		=> $data['sex'],
-			"email" 	=> $data['email'],
-			"phone" 	=> $data['phone'],
-			"city_id" 	=> $data['city_id'],
-			"birthday" 	=> $data['dob'],
-			"source" 	=> isset($data['source']) ? $data['source'] : '',
-			"applied_role" 	=> isset($data['applied_role']) ? $data['applied_role'] : '',
-			"applied_role_secondary" 	=> isset($data['applied_role_secondary']) ? $data['applied_role_secondary'] : '',
-			"company" 	=> isset($data['company']) ? $data['company'] : '',
-			"edu_institution" 	=> isset($data['edu_institution']) ? $data['edu_institution'] : '',
-			"edu_course"=> isset($data['edu_course']) ? $data['edu_course'] : '',
-			"edu_year" 	=> isset($data['edu_year']) ? $data['edu_year'] : '',
-			"address" 	=> isset($data['address']) ? $data['address'] : '',
-			"job_status"=> $data['job_status'],
-			"why_mad" 	=> $data['why_mad'],
-			"volunteering_experience" 	=> $data['volunteering_experience'],
-			"campaign" 	=> isset($data['campaign']) ? $data['campaign'] : '',
-			'user_type'	=> 'applicant',
-			"password" 	=> "pass"
+            "name"      => $data['name'],
+            "sex"       => $data['sex'],
+            "email"     => $data['email'],
+            "phone"     => $data['phone'],
+            "city_id"   => $data['city_id'],
+            "birthday"  => $data['dob'],
+            "source"    => isset($data['source']) ? $data['source'] : '',
+            "applied_role"  => isset($data['applied_role']) ? $data['applied_role'] : '',
+            "applied_role_secondary"    => isset($data['applied_role_secondary']) ? $data['applied_role_secondary'] : '',
+            "company"   => isset($data['company']) ? $data['company'] : '',
+            "edu_institution"   => isset($data['edu_institution']) ? $data['edu_institution'] : '',
+            "edu_course"=> isset($data['edu_course']) ? $data['edu_course'] : '',
+            "edu_year"  => isset($data['edu_year']) ? $data['edu_year'] : '',
+            "address"   => isset($data['address']) ? $data['address'] : '',
+            "job_status"=> $data['job_status'],
+            "why_mad"   => $data['why_mad'],
+            "volunteering_experience"   => $data['volunteering_experience'],
+            "campaign"  => isset($data['campaign']) ? $data['campaign'] : '',
+            'user_type' => 'applicant',
+            "password"  => "pass"
         ],
         'auth' => ['data.simulation@makeadiff.in', 'pass']
     ]);
@@ -62,13 +62,13 @@ try {
     // var_dump($log);
 } finally {
     if ($response and $result->getStatusCode() == 200) {
-    	echo $response; //json_encode(['status' => 'success', 'message' => "Volunteer Added"]);
-	} else {
-		echo json_encode(['status' => 'error', 'message' => 'Error adding your data - please try again.']);
-		if(!$log) {
-			$log = ['response' => $response, 'error' => "Status: " . $result->getStatusCode(), 'time' => date('Y-m-d H:i:s'), 'data' => $data, 'server' => $_SERVER];
-    		file_put_contents(__DIR__ . '/error.log', json_encode($log) . "\n", FILE_APPEND);
-    		// var_dump($log);
-		}
-	}
+        echo $response; //json_encode(['status' => 'success', 'message' => "Volunteer Added"]);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Error adding your data - please try again.']);
+        if(!$log) {
+            $log = ['response' => $response, 'error' => "Status: " . $result->getStatusCode(), 'time' => date('Y-m-d H:i:s'), 'data' => $data, 'server' => $_SERVER];
+            file_put_contents(__DIR__ . '/error.log', json_encode($log) . "\n", FILE_APPEND);
+            // var_dump($log);
+        }
+    }
 }
