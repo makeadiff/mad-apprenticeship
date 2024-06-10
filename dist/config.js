bd919c55226e158bc25cb6dafb33dcd9ef974578
Binny V A <binnyva@gmail.com>
2024-06-10 16:41:44 +0100

    Added redirect handling

--- dist/config.js
+++ dist/config.js
@@ -7,3 +7,36 @@ window.RM.config = {
 }
 
 window.chunkURL = "/apprenticeship/dist/";
+
+
+const customInit = function () {
+  // Copy over these parameters...
+  let redirect = "";
+
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+
+  let loc = document.location.href;
+
+  if(params.redirectTo) {
+    redirect = params.redirectTo;
+
+    redirect = decodeURI(redirect);
+    Object.keys(params)?.forEach(key => {
+    if (!key?.includes('redirectTo')) {
+      redirect = redirect + '&' + key + '=' + params[key]
+    }
+  })
+  }
+
+  if(redirect) {
+    const anchors = document.getElementsByTagName("a");
+    for(let i=0; i<anchors.length; i++) {
+      if(anchors[i].classList.contains('external')) continue; // Skip external links
+      anchors[i].href = redirect;
+    }
+
+  }
+}
+
+window.addEventListener("load", customInit, false); 
